2c6620535e7626d5afc54152f2e7b6cd86558393
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2022-09-07 18:18:25 -0700

    chore: update package.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
 
 import 'styles/index.css';
 import Routes from 'routes';
 
-const queryClient = new QueryClient();
-
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Routes />
-      </Router>
-    </QueryClientProvider>
+    <Router>
+      <Routes />
+    </Router>
   </React.StrictMode>,
 );
